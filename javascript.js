08dde30d3ef06dc79c7c61fc2063cb50fe49dfa3
Ola <bancolejeandedieu@gmail.com>
2024-12-06 11:32:19 +0100

    fix: styling finished

--- javascript.js
+++ javascript.js
@@ -4,12 +4,15 @@ function Book(auteur, titre, pages, read) {
     this.titre = titre;
     this.pages = pages;
     this.read = read;
+    this.changeReadStatut = function(){
+        this.read = !this.read;
+    }
 }
-
+//read the function name
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
-
+// mostly dialog stuffs
 function addingInteraction()
 {
     const addingButton = document.querySelector("#addingLogo");
@@ -30,33 +33,37 @@ function addingInteraction()
         createDivs();
         dialog.close();
     })
-
 }
-
+//show Details
 function showDetails(element){
     const bookTitle = document.querySelector("#bookTitle");
     const authorName = document.querySelector("#authorDisplay");
     const numberPage = document.querySelector("#pageDisplay");
-    const checkbox  = document.querySelector("#checkboxBook")
+    const readPara  = document.querySelector("#readOrNot p")
     bookTitle.textContent = `${element.titre}`;
     authorName.textContent = `Écrit par ${element.auteur}`;
     numberPage.textContent = `${element.pages} pages `;
     if(element.read)
-        checkbox.checked = true;
-    else
-        checkbox.checked = false;
+    {
+        readPara.textContent = `Finished`;
 
+    }
+    else
+    {
+        readPara.textContent = `Not Read`;
+    }
 }
+//divs creation
 function createDivs()
 {
     const principal = document.querySelector("#main");
     const dialogBook = document.querySelector("#dialogBook");
     const AnnulerBook = document.querySelector("#cancelFromBook");
     const supprimerBook = document.querySelector("#supprimerBook");
-
+    const toggleRead = document.querySelector("#toggleRead")
     principal.innerHTML = ""
     myLibrary.forEach((element, index) => {
-        let aDiv = document.createElement("div");
+        let aDiv = document.createElement("button");
         aDiv.classList.add("book");
         aDiv.textContent = `${element.titre}`;
         principal.appendChild(aDiv);
@@ -65,22 +72,25 @@ function createDivs()
             showDetails(element);
             
         } )
+        toggleRead.addEventListener("click", () => {
+            element.changeReadStatut();
+            console.log(element.read);
+            showDetails(element);
+        })
+        supprimerBook.addEventListener("click", () => {
+            dialogBook.close();
+        })
     });
-
-    AnnulerBook.addEventListener("click", () => {
+    AnnulerBook.addEventListener("click", () => { 
         dialogBook.close();
     })
-    supprimerBook.addEventListener("click", () => {
-        dialogBook.close();
-    })
-    
 }
-
-
-
+//main function
 function main(){
     const firstBook = new Book("6ix9ine", "How to be a rat?", 69, true);
+    const SecondBook = new Book("Robert Kiyosaki", "Poor dad, Rich dad", 336, true)
     addBookToLibrary(firstBook);
+    addBookToLibrary(SecondBook);
     createDivs();
     addingInteraction(); 
 }
